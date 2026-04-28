bf7fbcd4e08cacbac226b292a646d53d5484bbd6
Keyur Pathar <keyurpathar005@gmail.com>
2026-04-28 21:46:15 +0530

    top doctors section added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import SpecialitySec from '../components/SpecialitySec'
+import TopDoctors from '../components/TopDoctors'
 
 const Home = () => {
   return (
@@ -9,6 +10,8 @@ const Home = () => {
       <Hero />
 
       <SpecialitySec />
+
+      <TopDoctors />
       
     </div>
   )
