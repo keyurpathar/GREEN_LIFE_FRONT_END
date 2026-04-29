c375649028103a4bdbf39ef449b4d080074d3f34
Keyur Pathar <keyurpathar005@gmail.com>
2026-04-29 11:41:06 +0530

    banner section added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero'
 import SpecialitySec from '../components/SpecialitySec'
 import TopDoctors from '../components/TopDoctors'
+import Banner from '../components/Banner'
 
 const Home = () => {
   return (
@@ -12,6 +13,8 @@ const Home = () => {
       <SpecialitySec />
 
       <TopDoctors />
+
+      <Banner />
       
     </div>
   )
